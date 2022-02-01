3a81e0e5e68f7c0d5c21c97ba1240dadc7369d22
Stefan Turner <turner.stefan281@gmail.com>
2022-02-01 16:00:14 +0100

    add second box

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,4 @@
 import css from '../scss/main.scss';
+import { second } from './boxes/second.js';
+
+second();
---
+++ src/assets/js/boxes/second.js
@@ -0,0 +1,17 @@
+export const second = () => {
+    document.querySelector('.second__btn').addEventListener('click', () => {
+        let color1 = `rgb(${randomNum(0, 255)}, ${randomNum(0, 255)}, ${randomNum(0, 255)})`;
+        let color2 = `rgb(${randomNum(0, 255)}, ${randomNum(0, 255)}, ${randomNum(0, 255)})`;
+        let angle = randomNum(0, 360);
+    
+        document.querySelector('.second__text').innerHTML = `CSS<br>background: linear-gradient(${angle}deg, ${color1}, ${color2})`;
+    
+        document.querySelector('.second').style.backgroundImage = `linear-gradient(${angle}deg,${color1}, ${color2})`;
+    });
+
+    function randomNum(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min +1)) + min;
+      }
+};
